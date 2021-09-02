87e90cf6f2fa7a85aacfb290ed129a1eb52ee6e7
Nitai Bezerra da Silva <nitai.silva@economia.gov.br>
2021-09-02 08:42:17 -0300

    test(unit_test.py): add test_is_signature

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -15,12 +15,12 @@ sys.path.insert(0, parentdir)
 from dou_dag_generator import DouDigestDagGenerator
 
 @pytest.mark.parametrize('raw_html, clean_text',
-                          [
-                            ('<div><a>Any text</a></div>', 'Any text'),
-                            ('<div><div><p>Any text</a></div>', 'Any text'),
-                            ('Any text</a></div>', 'Any text'),
-                            ('Any text', 'Any text'),
-                            ])
+    [
+    ('<div><a>Any text</a></div>', 'Any text'),
+    ('<div><div><p>Any text</a></div>', 'Any text'),
+    ('Any text</a></div>', 'Any text'),
+    ('Any text', 'Any text'),
+    ])
 def test_clean_html(raw_html, clean_text):
     assert DouDigestDagGenerator().clean_html(raw_html) == clean_text
 
@@ -28,10 +28,12 @@ def test_clean_html(raw_html, clean_text):
     [
     ("PRIOR<>MATCHED NAME<>EVENTUALLY END NAME",
      "PRIOR",
-     "MATCHED NAME"),
+     "MATCHED NAME")
+    ,
     ("JOSE<span class='highlight' style='background:#FFA;'>ANTONIO DE OLIVEIRA</span>CAMARGO",
      "JOSE",
-     "ANTONIO DE OLIVEIRA"),
+     "ANTONIO DE OLIVEIRA")
+    ,
     ("<span class='highlight' style='background:#FFA;'>ANTONIO DE OLIVEIRA</span>CAMARGO",
      "",
      "ANTONIO DE OLIVEIRA"),
@@ -41,12 +43,26 @@ def test_get_prior_and_matched_name(raw_html, start_name, match_name):
         .get_prior_and_matched_name(raw_html) == (start_name, match_name)
 
 @pytest.mark.parametrize('raw_text, normalized_text',
-                          [
-                            ('Nitái Bêzêrrá', 'nitai bezerra'),
-                            ('Nitái-Bêzêrrá', 'nitai bezerra'),
-                            ('Normaliza çedilha', 'normaliza cedilha'),
-                            ('ìÌÒòùÙúÚáÁÀeççÇÇ~ A', 'iioouuuuaaaecccc a'),
-                            ('a  \|%&* /  aáá  3d_U', 'a aaa 3d u'),
-                            ])
+    [
+    ('Nitái Bêzêrrá', 'nitai bezerra'),
+    ('Nitái-Bêzêrrá', 'nitai bezerra'),
+    ('Normaliza çedilha', 'normaliza cedilha'),
+    ('ìÌÒòùÙúÚáÁÀeççÇÇ~ A', 'iioouuuuaaaecccc a'),
+    ('a  %&* /  aáá  3d_U', 'a aaa 3d u'),
+    ])
 def test_normalize(raw_text, normalized_text):
     assert DouDigestDagGenerator().normalize(raw_text) == normalized_text
+
+@pytest.mark.parametrize('search_term, abstract',
+    [
+    ("ANTONIO DE OLIVEIRA",
+     "<span class='highlight' style='background:#FFA;'>ANTONIO DE OLIVEIRA</span> FREITAS - Diretor Geral.EXTRATO DE COMPROMISSO PRONAS/PCD: Termo de Compromisso que entre si celebram a União, por intermédio do Ministério da Saúde,...")
+    ,
+    ("ANTONIO DE OLIVEIRA",
+     "JOSÉ <span class='highlight' style='background:#FFA;'>ANTONIO DE OLIVEIRA</span> FREITAS - Diretor Geral.EXTRATO DE COMPROMISSO PRONAS/PCD: Termo de Compromisso que entre si celebram a União, por intermédio do Ministério da Saúde,...")
+    ,
+    ("MATCHED NAME",
+     "PRIOR<>MATCHED NAME<>EVENTUALLY END NAME"),
+    ])
+def test_is_signature(search_term, abstract):
+    assert DouDigestDagGenerator().is_signature(search_term, abstract)
