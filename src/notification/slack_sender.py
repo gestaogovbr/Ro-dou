b0ca240ca4647d81d459ebd6e598661f3e74f369
Nitai Bezerra da Silva <nitaibezerra@protonmail.com>
2023-03-23 19:36:09 -0300

    feat(slack_sender): send all matches at once
    Using a special slack message api called block kit.

--- src/notification/slack_sender.py
+++ src/notification/slack_sender.py
@@ -7,34 +7,76 @@ class SlackSender(ISender):
 
     def __init__(self, specs) -> None:
         self.webhook_url = specs.slack_webhook
+        self.blocks = []
 
 
     def send(self, search_report: dict, report_date: str=None):
-        """Parse the content, and send message to Slack"""
-        blocks = []
+        """Parse the content, and send message to Discord"""
         for group, results in search_report.items():
             if group != 'single_group':
-                blocks.append(f'**Grupo: {group}**\n')
+                self._add_header(f'Grupo: {group}')
             for term, items in results.items():
-                if not items:
-                    continue
-                blocks.append(f'**Resultados para: {term}**\n')
-                for item in items:
-                    item_md = f"""
-                        ### [{item['title']}]({item['href']})
-                        {item['abstract']}
-                        {item['date']}"""
-                    blocks.append(item_md)
-
-        self._send_to_slack('\n'.join(blocks))
+                if items:
+                    self._add_header(f'Termo: {term}')
+                    for item in items:
+                        self._add_block(item)
+        self._flush()
 
 
-    def _send_to_slack(self, md_text: str):
-        data = {
-            'text': {
-                'type': 'mrkdwn',
-                'text': md_text
+    def _add_header(self, text):
+        self.blocks.append({
+            "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": text,
+                    "emoji": True
+                }
+            })
+
+
+    def _add_block(self, item):
+        self.blocks += [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": item['title']
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": item['abstract']
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Publicado em: 15/03/2023"
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Acessar publicação",
+                        "emoji": True
+                    },
+                    "value": "click_me_123",
+                    "url": item['href'],
+                    "action_id": "button-action"
+                }
+            },
+            {
+                "type": "divider"
             }
+        ]
+
+
+    def _flush(self):
+        data = {
+            'blocks': self.blocks
         }
         result = requests.post(self.webhook_url, json=data)
         result.raise_for_status()
